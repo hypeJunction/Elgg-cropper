3f0b3f5d51e06ab619c4f76192ec8b3816792bc0
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-07 15:57:15 +0200

    migrate(3.x): fix elgg_get_simplecache_url two-arg to one-arg call

--- start.php
+++ start.php
@@ -24,7 +24,7 @@ function cropper_init() {
 	elgg_unregister_css('jquery.cropper');
 
 	elgg_register_simplecache_view('js/cropper.js');
-	$src = elgg_get_simplecache_url('js', 'cropper.js');
+	$src = elgg_get_simplecache_url('js/cropper.js');
 	elgg_register_js('jquery.cropper', $src);
 	elgg_define_js('cropper', [
 		'src' => $src,
